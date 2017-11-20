f310be0b55432e757221cb7534c548ebc404b3af
Bartosz Rogoz <BartoszRogoz@wp.pl>
2017-11-20 13:52:10 +0000

    Uruchomienie serwera z natywną obługą JS, poprawan walidacja zapytań

---
+++ App2.js
@@ -0,0 +1,31 @@
+var express = require('express');  
+var path = require('path');  
+var app = express();  
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var pinToogle = require('./routes/pinToogle');
+var loginpage =require('./routes/loginpage');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);  
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('views', path.join(__dirname, 'views'));  
+app.set('view engine', 'jade');
+
+
+
+router.all('/', function (req, res, next) {  
+  console.log('Someone made a request!');
+  next();
+});
+
+
+
+
+
+app.listen(9000);  
+module.exports = app;  
